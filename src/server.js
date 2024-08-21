204843091aa599056beefb2046d02bb9b085b904
yasmin gil <vizeugilyasmin@gmail.com>
2024-08-21 20:38:01 -0300

    ajuste deploy

--- src/server.js
+++ src/server.js
@@ -1,14 +1,19 @@
 import express from 'express'
 import userRouter from './routes/userRouter.js'
 import productRouter from './routes/productRouter.js'
+import { ENVIRONMENT, HOST, PORT } from './routes/config.js'
 
 const app = express()
-const port = 3000
+const port = PORT
+const host = HOST
+const environment = ENVIRONMENT
 
 app.use('/user', userRouter)
 app.use('/produto', productRouter)
 
 
-app.listen(3000, () => {
-    console.log('Servidor Rodando em http://localhost:3000')
+app.listen(PORT, () => {
+
+    
+    console.log(`Servidor Rodando no ambiente ${environment} em ${ environment == 'production' ? host : host+':'+port }`);
 })
